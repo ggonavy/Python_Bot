c8d014efdc2365bb43f2ffb3fbc845ba6229c9ce
ggonavy <84876475+ggonavy@users.noreply.github.com>
2025-06-27 20:58:33 -0400

    Slate_PythonBot_v12_Strategy.py

--- Slate_PythonBot_v12_Strategy.py
+++ Slate_PythonBot_v12_Strategy.py
@@ -2,8 +2,8 @@ import krakenex
 from pykrakenapi import KrakenAPI
 
 api = krakenex.API()
-api.key = "YOUR_API_KEY"
-api.secret = "YOUR_API_SECRET"
+api.key = "haDXxKlf3s04IL8OZsBy5j+kn7ZTS8LjnkwZvHjpmL+0sYZj8IfwxniM"
+api.secret = "MvohzPBpHaG0S3vxrMtldcnGFoa+9cXLvJ8IxrwwOduSDaLgxPxG2YK/9cRQCEOnYoSmR22ZzUJr4CPIXDh19Q=="
 k = KrakenAPI(api)
 
 balances = k.get_account_balance()
